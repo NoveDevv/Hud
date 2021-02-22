484baffb8dac38af910b49adae6be29eda659aed
NoveDevv <78967243+NoveDevv@users.noreply.github.com>
2021-02-22 18:21:29 +0100

    Add files via upload

---
+++ Hud/ReadMe.txt
@@ -0,0 +1,8 @@
+Salut a tous voici l'hud de red side reconvertie en esx par moi 
+Pour tout problème me MP
+
+Discord : Nove.exe#9999
+
+CoreV : https://discord.gg/6ttY4Emau2
+
+Nove - Discord : https://discord.gg/Q5vkgCqgSf
---
+++ Hud/__resource.lua
@@ -0,0 +1,78 @@
+fx_version 'bodacious'
+game 'CoreV'
+
+
+
+ui_page 'html/ui.html'
+
+
+
+client_scripts {
+
+    '@es_extended/locale.lua',
+
+	'client.lua',	
+
+}
+
+
+
+files {
+
+	'html/ui.html',
+
+	'html/style.css',
+
+	'html/grid.css',
+
+	'html/main.js',
+
+	'html/img/bank.png',
+
+	'html/img/dirtymoney.png',
+
+	'html/img/money.png',
+
+	'html/img/jobs/unemployed.png',
+
+	'html/img/jobs/unemployed2.png',
+
+	'html/img/jobs/police.png',
+
+	'html/img/jobs/concess.png',
+
+	'html/img/jobs/taxi.png',
+
+	'html/img/jobs/ambulance.png',
+
+	'html/img/jobs/mecano.png',
+
+	'html/img/jobs/cardealer.png',
+
+	'html/img/jobs/realestateagent.png',
+
+	'html/img/jobs/sheriff.png',
+
+	'html/img/jobs/drusillas.png',
+
+	'html/img/jobs/ballas.png',
+
+	'html/img/jobs/families.png',
+
+	'html/img/jobs/vagos.png',
+
+	'html/img/jobs/marabunta.png',
+
+	'html/img/jobs/biker.png',
+
+	'html/img/jobs/bratva.png',
+
+	'html/img/jobs/vigne.png',
+
+	'html/img/jobs/tabac.png'
+
+
+
+}
+
+export 'SetInstructionalButton'
